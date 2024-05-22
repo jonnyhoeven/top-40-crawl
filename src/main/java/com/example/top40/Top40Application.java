81f8d7b69d22d276e2fcf0463a2eec5a2b1545d6
Jonny van der Hoeven [NIPV] <jonny.vanderhoeven@nipv.nl>
2024-05-22 09:15:23 +0200

    init spring

---
+++ src/main/java/com/example/top40/Top40Application.java
@@ -0,0 +1,13 @@
+package com.example.top40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Top40Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/top40/Top40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.top40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Top40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
